7a195f4490df4604ad3373973cf0733f416b5d0b
Yosuke Hara <yousuke.hara@gmail.com>
2013-06-17 14:28:33 +0900

    Revise default # of vnodes for the test

--- include/leo_redundant_manager.hrl
+++ include/leo_redundant_manager.hrl
@@ -82,7 +82,7 @@
 -define(DEF_OPT_D, 1).
 -define(DEF_OPT_BIT_OF_RING, ?MD5).
 -ifdef(TEST).
--define(DEF_NUMBER_OF_VNODES, 128).
+-define(DEF_NUMBER_OF_VNODES, 168).
 -else.
 -define(DEF_NUMBER_OF_VNODES, 168).
 -endif.
