f371fd482ac2c4aa8d9838bfa136f1ae17e68242
Yosuke Hara <yousuke.hara@gmail.com>
2014-01-15 19:10:40 +0900

    Update copy-right

--- src/leo_redundant_manager_chash.erl
+++ src/leo_redundant_manager_chash.erl
@@ -2,7 +2,8 @@
 %%
 %% Leo Redundant Manager
 %%
-%% Copyright (c) 2012-2013 Rakuten, Inc.
+%% Copyright (c) 2012-2014 Rakuten, Inc.
+%%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
 %% except in compliance with the License.  You may obtain
