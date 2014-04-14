9bb1b883391c3f2f6f28ea826c41a199acc9e3da
Yosuke Hara <yousuke.hara@gmail.com>
2014-04-14 19:26:21 +0900

    Fix leo-project/leofs/issues/173

--- src/leo_mdcr_tbl_cluster_info.erl
+++ src/leo_mdcr_tbl_cluster_info.erl
@@ -119,7 +119,7 @@ find_by_limit(Rows) ->
 find_by_limit(Rows, Acc) when Rows == length(Acc) ->
     {ok, Acc};
 find_by_limit(Rows, []) ->
-    Table =  ?TBL_CLUSTER_INFO,
+    Table = ?TBL_CLUSTER_INFO,
     case catch mnesia:ets(fun ets:first/1, [Table]) of
         {'EXIT', Cause} ->
             {error, Cause};
@@ -138,7 +138,7 @@ find_by_limit(Rows, []) ->
 find_by_limit(Rows,_ClusterId, Acc) when Rows == length(Acc) ->
     {ok, Acc};
 find_by_limit(Rows, ClusterId, Acc) ->
-    Table =  ?TBL_CLUSTER_INFO,
+    Table = ?TBL_CLUSTER_INFO,
     case catch mnesia:ets(fun ets:next/2, [Table, ClusterId]) of
         {'EXIT', Cause} ->
             {error, Cause};
@@ -287,6 +287,12 @@ transform() ->
                      ?TBL_CLUSTER_INFO,  fun transform/1,
                      record_info(fields, ?CLUSTER_INFO),
                      ?CLUSTER_INFO),
+    case (?MODULE:size() > 0) of
+        true ->
+            transform_1();
+        false ->
+            void
+    end,
     ok.
 
 
@@ -303,16 +309,8 @@ transform(#cluster_info{cluster_id = ClusterId,
                         bit_of_ring = BitOfRing,
                         num_of_dc_replicas   = Level1,
                         num_of_rack_replicas = Level2}) ->
-    ClusterId_1 = case is_atom(ClusterId) of
-                      true  -> ClusterId;
-                      false -> list_to_atom(ClusterId)
-                  end,
-    DCId_1 = case is_atom(DCId) of
-                 true  -> DCId;
-                 false -> list_to_atom(DCId)
-             end,
-    #?CLUSTER_INFO{cluster_id = ClusterId_1,
-                   dc_id = DCId_1,
+    #?CLUSTER_INFO{cluster_id = ClusterId,
+                   dc_id = DCId,
                    n = N,
                    r = R,
                    w = W,
@@ -322,3 +320,57 @@ transform(#cluster_info{cluster_id = ClusterId,
                    num_of_rack_replicas = Level2,
                    max_mdc_targets = ?DEF_MAX_MDC_TARGETS
                   }.
+
+
+%% @private
+transform_1() ->
+    Table = ?TBL_CLUSTER_INFO,
+    case catch mnesia:ets(fun ets:first/1, [Table]) of
+        {'EXIT', Cause} ->
+            {error, Cause};
+        '$end_of_table' ->
+            ok;
+        Key ->
+            case transform_2(Key) of
+                ok ->
+                    transform_1(Key);
+                _Error ->
+                    ok
+            end
+    end.
+transform_1(PrevKey) ->
+    Table = ?TBL_CLUSTER_INFO,
+    case catch mnesia:ets(fun ets:next/2, [Table, PrevKey]) of
+        {'EXIT', Cause} ->
+            {error, Cause};
+        '$end_of_table' ->
+            ok;
+        Key ->
+            case transform_2(Key) of
+                ok ->
+                    transform_1(Key);
+                _Error ->
+                    ok
+            end
+    end.
+
+%% @private
+transform_2(Key) ->
+    case ?MODULE:get(Key) of
+        {ok, #?CLUSTER_INFO{cluster_id = ClusterId,
+                            dc_id = DCId} = ClusterInfo} ->
+            ClusterId_1 = case is_atom(ClusterId) of
+                              true  -> ClusterId;
+                              false -> list_to_atom(ClusterId)
+                          end,
+            DCId_1 = case is_atom(DCId) of
+                         true  -> DCId;
+                         false -> list_to_atom(DCId)
+                     end,
+            ?MODULE:delete(ClusterId),
+            ?MODULE:update(ClusterInfo#?CLUSTER_INFO{cluster_id = ClusterId_1,
+                                                     dc_id = DCId_1}),
+            ok;
+        _ ->
+            void
+    end.
