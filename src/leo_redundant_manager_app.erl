38e15bc4b93f5419264927cabc71180dd8fb009d
Yosuke Hara <yousuke.hara@gmail.com>
2014-05-07 13:30:37 +0900

    Modify to stop the application

--- src/leo_redundant_manager_app.erl
+++ src/leo_redundant_manager_app.erl
@@ -31,7 +31,8 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--export([start/2, stop/1, profile_output/0]).
+-export([start/2, prep_stop/1, stop/1,
+         profile_output/0]).
 
 %%----------------------------------------------------------------------
 %% Application behaviour callbacks
@@ -39,6 +40,10 @@
 start(_Type, _Args) ->
     consider_profiling(),
     leo_redundant_manager_sup:start_link().
+
+prep_stop(_State) ->
+    ok = leo_redundant_manager_sup:stop(),
+    ok.
 
 stop(_State) ->
     ok.
