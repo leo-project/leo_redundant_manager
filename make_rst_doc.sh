f8d164bd6ee6c00cd7c36633cdf445ef80d89977
Yosuke Hara <yousuke.hara@gmail.com>
2014-10-02 14:01:47 +0900

    Modify script of making docs

--- make_rst_doc.sh
+++ make_rst_doc.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
 
-rm -rf doc/rst && mkdir doc/rst
 make doc
-pandoc --read=html --write=rst doc/leo_cluster_tbl_conf.html -o doc/rst/leo_cluster_tbl_conf.rst
-pandoc --read=html --write=rst doc/leo_cluster_tbl_member.html -o doc/rst/leo_cluster_tbl_member.rst
-pandoc --read=html --write=rst doc/leo_cluster_tbl_ring.html -o doc/rst/leo_cluster_tbl_ring.rst
-pandoc --read=html --write=rst doc/leo_cluster_tbl_sync_behaviour.html -o doc/rst/leo_cluster_tbl_sync_behaviour.rst
-pandoc --read=html --write=rst doc/leo_mdcr_tbl_cluster_info.html -o doc/rst/leo_mdcr_tbl_cluster_info.rst
-pandoc --read=html --write=rst doc/leo_mdcr_tbl_cluster_member.html -o doc/rst/leo_mdcr_tbl_cluster_member.rst
-pandoc --read=html --write=rst doc/leo_mdcr_tbl_cluster_mgr.html -o doc/rst/leo_mdcr_tbl_cluster_mgr.rst
-pandoc --read=html --write=rst doc/leo_mdcr_tbl_cluster_stat.html -o doc/rst/leo_mdcr_tbl_cluster_stat.rst
-pandoc --read=html --write=rst doc/leo_mdcr_tbl_sync.html -o doc/rst/leo_mdcr_tbl_sync.html
-pandoc --read=html --write=rst doc/leo_membership_cluster_local.html -o doc/rst/leo_membership_cluster_local.rst
-pandoc --read=html --write=rst doc/leo_membership_cluster_remote.html -o doc/rst/leo_membership_cluster_remote.rst
-pandoc --read=html --write=rst doc/leo_membership_mq_client.html -o doc/rst/leo_membership_mq_client.rst
-pandoc --read=html --write=rst doc/leo_redundant_manager.html -o doc/rst/leo_redundant_manager.rst
-pandoc --read=html --write=rst doc/leo_redundant_manager_api.html -o doc/rst/leo_redundant_manager_api.rst
-pandoc --read=html --write=rst doc/leo_redundant_manager_chash.html -o doc/rst/leo_redundant_manager_chash.rst
-pandoc --read=html --write=rst doc/leo_redundant_manager_worker.html -o doc/rst/leo_redundant_manager_worker.rst
-pandoc --read=html --write=rst doc/leo_ring_tbl_transformer.html -o doc/rst/leo_ring_tbl_transformer.rst
+rm -rf doc/rst && mkdir doc/rst
+
+for Mod in leo_cluster_tbl_conf \
+           leo_cluster_tbl_member \
+           leo_cluster_tbl_ring \
+           leo_mdcr_tbl_cluster_info \
+           leo_mdcr_tbl_cluster_member \
+           leo_mdcr_tbl_cluster_mgr \
+           leo_mdcr_tbl_cluster_stat \
+           leo_mdcr_tbl_sync \
+           leo_membership_cluster_local \
+           leo_membership_cluster_remote \
+           leo_membership_mq_client \
+           leo_redundant_manager \
+           leo_redundant_manager_api \
+           leo_redundant_manager_chash \
+           leo_redundant_manager_worker \
+           leo_ring_tbl_transformer
+do
+    read_file="doc/$Mod.html"
+    write_file="doc/rst/$Mod.rst"
+
+    pandoc --read=html --write=rst "$read_file" -o "$write_file"
+
+    sed -ie "1,6d" "$write_file"
+    sed -ie "s/\Module //" "$write_file"
+    LINE_1=`cat $write_file | wc -l`
+    LINE_2=`expr $LINE_1 - 10`
+    sed -ie "$LINE_2,\$d" "$write_file"
+done
+rm -rf doc/rst/*.rste
